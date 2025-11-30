6c5b96d71fb39480911519c090a68976940dbd13
7hszxxcdvp-max <7hszxxcdvp@privaterelay.appleid.com>
2025-11-30 16:04:04 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,17 +1,2 @@
-// Import des modules Firebase 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"; 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js"; 
-
-// Configuration de ton projet Firebase 
-const firebaseConfig = { 
-    apiKey: "TON_API_KEY", 
-    authDomain: "TON_PROJET.firebaseapp.com", 
-    projectId: "TON_PROJET", 
-    storageBucket: "TON_PROJET.appspot.com", 
-    messagingSenderId: "TON_ID", 
-    appId: "TON_APP_ID" 
-}; 
-
-// Initialiser Firebase 
-const app = initializeApp(firebaseConfig); 
-const db = getFirestore(app);
+// Placeholder pour ton code Firebase
+console.log("Script chargé correctement !");
